38b3bc18fc0753db8c6c42fc4b8dbeaccd859fc7
usuario <usuario@macbook-pro.local>
2019-01-09 11:35:48 -0600

    Se agrega funcionalidad de recuperar contraseña

--- Actividad1/InfoRecuperarViewController.swift
+++ Actividad1/InfoRecuperarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  OlvideViewController.swift
+//  InfoRecuperarViewController.swift
 //  Actividad1
 //
 //  Created by usuario on 1/9/19.
@@ -8,10 +8,8 @@
 
 import UIKit
 
-class OlvideViewController: UIViewController {
+class InfoRecuperarViewController: UIViewController {
 
-    @IBOutlet weak var correoRecupera: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,13 +17,14 @@ class OlvideViewController: UIViewController {
     }
     
 
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
-    
+    */
 
 }
